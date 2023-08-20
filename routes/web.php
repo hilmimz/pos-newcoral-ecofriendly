fe93536a265e23b78a733f4a8f6577dde754eed2
Hilmi Muzhaffar <hilmimuzhaffar15@student.uns.ac.id>
2023-08-20 20:58:24 +0700

    redirect to home

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('home');
 });
 
 Route::middleware(['guest'])->group(function () {
